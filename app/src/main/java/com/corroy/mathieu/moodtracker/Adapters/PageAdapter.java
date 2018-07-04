003cb2a5c3b47b181d72c5948ee36e2daea73723
Mathieu72000 <Math.corroy@gmail.com>
2018-07-04 13:37:14 +0200

    Add new things

--- app/src/main/java/com/corroy/mathieu/moodtracker/Adapters/PageAdapter.java
+++ app/src/main/java/com/corroy/mathieu/moodtracker/Adapters/PageAdapter.java
@@ -1,1 +1,40 @@
 package com.corroy.mathieu.moodtracker.Adapters;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import com.corroy.mathieu.moodtracker.Fragments.MoodFragment;
+
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+    public PageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    // Return view numbers
+    @Override
+    public int getCount(){
+        return 5;
+    }
+
+    // Return the elements position
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0 :
+                return MoodFragment.newInstance("sad", "faded_red");
+            case 1 :
+                return MoodFragment.newInstance("disappointed", "warm_grey");
+            case 2 :
+                return MoodFragment.newInstance("normal", "cornflower_blue_65");
+            case 3 :
+                return MoodFragment.newInstance("happy", "light_sage");
+            case 4 :
+                return MoodFragment.newInstance("super_happy", "banana_yellow");
+            default :
+                return null;
+        }
+    }
+}
--- app/src/main/java/com/corroy/mathieu/moodtracker/Fragments/MoodFragment.java
+++ app/src/main/java/com/corroy/mathieu/moodtracker/Fragments/MoodFragment.java
@@ -5,17 +5,48 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import com.corroy.mathieu.moodtracker.R;
 
 
 public class MoodFragment extends Fragment {
 
+    private static final String MOOD = "mood";
+    private static final String BACKGROUND = "background";
 
-    // Empty public constructor
-    public MoodFragment() {
+    private String mMood;
+    private String mBackground;
+    private View mView;
+    private ImageView mImageView;
+
+    // New instance of MoodFragment for the mood and the background color
+    public static MoodFragment newInstance(String mood, String background) {
+        MoodFragment moodFragment = new MoodFragment();
+
+        // New Bundle for supply mood and background as arguments
+        Bundle bdl = new Bundle();
+        bdl.putString(MOOD, mood);
+        bdl.putString(BACKGROUND, background);
+        moodFragment.setArguments(bdl);
+        return moodFragment;
     }
 
+    // When creating, retrieve this instance's mood and background color from its arguments
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMood = getArguments() != null ? getArguments().getString(MOOD) : "happy";
+        mBackground = getArguments() != null ? getArguments().getString(BACKGROUND) : "light_sage";
+    }
 
+    // The user interface show its instance mood and background color
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_mood, container, false);
+        mView.setBackgroundColor(getResources().getColor(getResources().getIdentifier(mBackground, "color", getActivity().getPackageName())));
+
+        mImageView = mView.findViewById(R.id.mood_img);
+        mImageView.setImageResource(getResources().getIdentifier("smiley_" + mMood, "drawable", getActivity().getPackageName()));
+        return mView;
     }
 }
