78c041534ddc397b35a6ce00402e2269adba71e7
Mathieu72000 <Math.corroy@gmail.com>
2018-07-05 14:34:15 +0200

    Add the onCreate method with Arguments Set the user Interface with the onCreateView method

--- app/src/main/java/com/corroy/mathieu/moodtracker/Fragments/MoodFragment.java
+++ app/src/main/java/com/corroy/mathieu/moodtracker/Fragments/MoodFragment.java
@@ -2,8 +2,12 @@ package com.corroy.mathieu.moodtracker.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.corroy.mathieu.moodtracker.R;
 
 
 public class MoodFragment extends Fragment {
@@ -26,5 +30,24 @@ public class MoodFragment extends Fragment {
         bdl.putString(BACKGROUND, background);
         moodFragment.setArguments(bdl);
         return moodFragment;
+    }
+
+    // When creating, retrieve this instance's mood and background color from its arguments
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mMood = getArguments() != null ? getArguments().getString(MOOD) : "happy";
+        mBackground = getArguments() != null ? getArguments().getString(BACKGROUND) : "light_sage";
+    }
+
+    // The user interface show its instance mood and background color
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_mood, container, false);
+        mView.setBackgroundColor(getResources().getColor(getResources().getIdentifier(mBackground, "color", getActivity().getPackageName())));
+
+        mImageView = mView.findViewById(R.id.mood_img);
+        mImageView.setImageResource(getResources().getIdentifier("smiley_" + mMood, "drawable", getActivity().getPackageName()));
+        return mView;
     }
 }
