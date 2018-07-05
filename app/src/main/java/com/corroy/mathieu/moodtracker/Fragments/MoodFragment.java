edb8176aad9af0e4f0529504f2921467f4f2f563
Mathieu72000 <Math.corroy@gmail.com>
2018-07-05 14:32:17 +0200

    Add new instance of MoodFragment and a Bundle

--- app/src/main/java/com/corroy/mathieu/moodtracker/Fragments/MoodFragment.java
+++ app/src/main/java/com/corroy/mathieu/moodtracker/Fragments/MoodFragment.java
@@ -2,20 +2,29 @@ package com.corroy.mathieu.moodtracker.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.ImageView;
 
 
 public class MoodFragment extends Fragment {
 
+    private static final String MOOD = "mood";
+    private static final String BACKGROUND = "background";
 
-    // Empty public constructor
-    public MoodFragment() {
-    }
+    private String mMood;
+    private String mBackground;
+    private View mView;
+    private ImageView mImageView;
 
+    // New instance of MoodFragment for the mood and the background color
+    public static MoodFragment newInstance(String mood, String background) {
+        MoodFragment moodFragment = new MoodFragment();
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // New Bundle for supply mood and background as arguments
+        Bundle bdl = new Bundle();
+        bdl.putString(MOOD, mood);
+        bdl.putString(BACKGROUND, background);
+        moodFragment.setArguments(bdl);
+        return moodFragment;
     }
 }
