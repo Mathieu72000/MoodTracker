0804968b0163019adfacb7bd6b9f134c7b21706a
Mathieu72000 <Math.corroy@gmail.com>
2018-09-06 17:00:43 +0200

    Add a new object named "MoodEntry" Add a Database for the historic add a new Enumeration New design for the historic activity

--- app/src/main/java/com/corroy/mathieu/moodtracker/Controllers/MainActivity.java
+++ app/src/main/java/com/corroy/mathieu/moodtracker/Controllers/MainActivity.java
@@ -5,13 +5,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
 import com.corroy.mathieu.moodtracker.Adapters.PageAdapter;
+import com.corroy.mathieu.moodtracker.Models.HistoryDataBase;
+import com.corroy.mathieu.moodtracker.Models.MoodEntry;
 import com.corroy.mathieu.moodtracker.R;
-
+import java.util.Date;
 
 public class MainActivity extends FragmentActivity {
 
@@ -19,6 +21,10 @@ public class MainActivity extends FragmentActivity {
     private ImageButton mCommentBtn;
     private ImageButton mHistoryBtn;
     private String m_Text = "";
+    private MoodEntry mood = new MoodEntry();
+    private HistoryDataBase mHistoryDatabase = new HistoryDataBase(this);
+    private int moodState = 3 ;
+    Date date = new Date();
 
     // Application launching
     @Override
@@ -29,8 +35,6 @@ public class MainActivity extends FragmentActivity {
         mVerticalViewPager = findViewById(R.id.activity_main_view_pager);
         mCommentBtn = findViewById(R.id.activity_comment_btn);
         mHistoryBtn = findViewById(R.id.activity_history_btn);
-
-
         mCommentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,16 +43,22 @@ public class MainActivity extends FragmentActivity {
                 builder.setTitle("What happened today ?");
 
                 final EditText input = new EditText(MainActivity.this);
-                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                input.setText(mood.getNote());
                 builder.setView(input);
 
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                builder.setPositiveButton("VALIDER", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         m_Text = input.getText().toString();
+                        mood.setNote(m_Text);
+                        Toast.makeText(getApplicationContext(), R.string.comment_saved, Toast.LENGTH_LONG).show();
+                        mVerticalViewPager.getCurrentItem();
+                        mHistoryDatabase.addMood(mood);
+                        mHistoryDatabase.updateMood(mood);
+                        mHistoryDatabase.close();
                     }
                 });
-                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                builder.setNegativeButton("ANNULER", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.cancel();
@@ -68,20 +78,35 @@ public class MainActivity extends FragmentActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("MainActivity - onStart");
+    }
+
     // Application is running
     @Override
     protected void onResume() {
         super.onResume();
+        mHistoryDatabase = new HistoryDataBase(this);
+        MoodEntry currMood = mHistoryDatabase.getMood(date);
+        if(currMood != null) {
+            m_Text = currMood.getNote();
+        } else {
+            moodState = 3;
+            m_Text = "";
+        }
+        mHistoryDatabase.close();
         // Set the PagerAdapter to show fragments
         mVerticalViewPager.setAdapter(new PageAdapter(getSupportFragmentManager()));
-        mVerticalViewPager.setCurrentItem(3, true);
+        mVerticalViewPager.setCurrentItem(moodState);
         System.out.println("MainActivity - onResume");
     }
 
     @Override
-    protected void onStart(){
-        super.onStart();
-        System.out.println("MainActivity - onStart");
+    protected void onPause() {
+        super.onPause();
+        System.out.println("MainActivity - onPause");
     }
 
     @Override
